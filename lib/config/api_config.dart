b7d262395f5b8eb46e5ce746eec64e3604fbad4c
rafhoerdeft <rafhoerdeft@gmail.com>
2020-12-07 10:42:21 +0700

    api config

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,4 +1,5 @@
-String api_rtlh = "http://192.168.118.166";
+// String api_rtlh = "http://192.168.118.166";
+String api_rtlh = "http://103.115.104.245";
 
 Map<String, String> requestHeader = {
   'Content-type': 'application/json',
