1b9afee20479c5ad351d411ac2fbf83f7a3c72cc
rafhoerdeft <rafhoerdeft@gmail.com>
2021-01-13 16:06:26 +0700

    change API url

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 // String api_rtlh = "http://192.168.118.166";
-String api_rtlh = "http://103.115.104.245/simpkp";
+String api_rtlh = "https://simpkp.magelangkab.go.id/simpkp";
 
 Map<String, String> requestHeader = {
   'Content-type': 'application/json',
